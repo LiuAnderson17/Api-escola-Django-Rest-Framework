9c7fb51580445ec4fc24088e607b05c4835ef822
Luan <liu.anderson17@gmail.com>
2025-11-05 17:07:20 -0300

    Segundo commit

--- escola/admin.py
+++ escola/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from escola.models import Estudante,Curso
+from escola.models import Estudante,Curso, Matricula
 
 class Estudantes(admin.ModelAdmin):
     list_display = ('id','nome','email','cpf','data_nascimento','celular')
     list_display_links = ('id','nome',)
     list_per_page = 20
-    search_fields = ('nome',)
+    search_fields = ('nome', 'cpf',)
+    ordering = ('nome',)
 
 admin.site.register(Estudante,Estudantes)
 
@@ -15,3 +16,9 @@ class Cursos(admin.ModelAdmin):
     search_fields = ('codigo',)
 
 admin.site.register(Curso,Cursos)
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id','estudante','curso','periodo')
+    list_display_links = ('id',)
+
+admin.site.register(Matricula,Matriculas)
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from escola.views import EstudanteViewSet,CursoViewSet,MatriculaViewSet,ListaMatriculaEstudante,ListaMatriculaCurso
+from escola.views import EstudanteViewSet,CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante,ListaMatriculaCurso
 from rest_framework import routers
 
 router = routers.DefaultRouter()
